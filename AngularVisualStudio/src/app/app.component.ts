20e9dd7531fb7e806098b3b0c77c4a7da6d95e46
masteraco <alfredoluis.torres@gmail.com>
2024-12-30 15:14:07 -0500

    Add project files.

---
+++ AngularVisualStudio/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'Angular in VisualStudio 2022 community by Alfredo Torres';
+}
